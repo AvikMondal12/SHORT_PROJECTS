947ebf5cd893fe942e893e9f6e4f91a0e1133161
Coder-Avik <128571756+AvikMondal12@users.noreply.github.com>
2024-09-30 02:55:25 +0530

    Add files via upload

---
+++ my_portfolio_website/js/script.js
@@ -0,0 +1,7 @@
+// typing Animation
+var typed =new Typed(".typing",{
+    strings:[" ","Student Of Brainware University &#x1F92B;","Bachelor Of Computer Applications &#x1F5A4;","From at DEPARA &#x1F607 &#x1F608;","Web Designer","Software Developer","Web developer","Coding Lover &#128157; &#x1F60C;"],
+    typespeed:60,
+    Backspeed:50,
+    loop:true
+})
